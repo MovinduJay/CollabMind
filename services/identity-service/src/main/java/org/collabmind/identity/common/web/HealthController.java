93b3af7d5f0a68832e7af79dcdd5e9d4009016d3
Movindu Jayathilake <movidujayathilake@gmail.com>
2026-08-31 21:07:19 +0530

    Fix pom issue

--- services/identity-service/src/main/java/org/collabmind/identity/common/web/HealthController.java
+++ services/identity-service/src/main/java/org/collabmind/identity/common/web/HealthController.java
@@ -1,10 +1,10 @@
 package org.collabmind.identity.common.web;
 
-import java.time.Instant;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Map;
 
 @RestController
 public class HealthController {
